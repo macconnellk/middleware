7bfc9d55e1aba8b2decffa6b6b57e86912c851e1
macconnellk <69541225+macconnellk@users.noreply.github.com>
2023-12-21 14:44:57 -0500

    Update sigmoid_adjustableTDDresponse.js
    Update log variables

--- sigmoid_adjustableTDDresponse.js
+++ sigmoid_adjustableTDDresponse.js
@@ -74,11 +74,11 @@ function middleware(iob, temp_basal, glucose, profile, autosens, meal, reservoir
 
    const tdd_weightedAverage = oref2_variables.weightedAverage;
    const tdd_average_total_data = oref2_variables.average_total_data;
-      var log_weightedAverage = "Log: tdd_weightedAverage: " + tdd_weightedAverage; 
-      var log_average_total_data = "Log: tdd_average_total_data: " + tdd_average_total_data;
+      log_weightedAverage = "Log: tdd_weightedAverage: " + tdd_weightedAverage; 
+      log_average_total_data = "Log: tdd_average_total_data: " + tdd_average_total_data;
     
    const tdd_dev = (tdd_weightedAverage / tdd_average_total_data - 1) * 10;
-    var log_tdd_dev = "Log: tdd_dev: " + tdd_dev;
+    log_tdd_dev = "Log: tdd_dev: " + tdd_dev;
 
     // Hard-code TDD Factor Sigmoid inputs
     // These inputs have been modeled to create a TDD Factor that, when used in the Sigmoid DynISF function, closely approximates the TDD delta effect for ULTRA-RAPID used in the Chris Wilson (Logarithmic) DynISF approach. 
@@ -87,33 +87,33 @@ function middleware(iob, temp_basal, glucose, profile, autosens, meal, reservoir
     const TDD_sigmoid_adjustment_factor = .42;
     const TDD_sigmoid_max = 4;
     const TDD_sigmoid_min = .7;
-      var log_TDD_sigmoid_adjustment_factor = ", Log: TDD_sigmoid_adjustment_factor: " + TDD_sigmoid_adjustment_factor;
-      var log_TDD_sigmoid_max = ", Log: TDD_sigmoid_max: " + TDD_sigmoid_max;
-      var log_TDD_sigmoid_min = ", Log: TDD_sigmoid_min: " + TDD_sigmoid_min;
-       return log_tdd_dev + log_TDD_sigmoid_adjustment_factor + log_TDD_sigmoid_max + log_TDD_sigmoid_min;
+      log_TDD_sigmoid_adjustment_factor = ", Log: TDD_sigmoid_adjustment_factor: " + TDD_sigmoid_adjustment_factor;
+      log_TDD_sigmoid_max = ", Log: TDD_sigmoid_max: " + TDD_sigmoid_max;
+      log_TDD_sigmoid_min = ", Log: TDD_sigmoid_min: " + TDD_sigmoid_min;
+       return log_weightedAverage + log_average_total_data + log_tdd_dev + log_TDD_sigmoid_adjustment_factor + log_TDD_sigmoid_max + log_TDD_sigmoid_min;
 
     // Define a TDD Factor Sigmoid curve using same method as the DynISF Sigmoid approach below
     const TDD_sigmoid_interval = TDD_sigmoid_max - TDD_sigmoid_min;
     const TDD_sigmoid_max_minus_one = TDD_sigmoid_max - 1;
     const TDD_sigmoid_fix_offset = (Math.log10(1/TDD_sigmoid_max_minus_one - TDD_sigmoid_min / TDD_sigmoid_max_minus_one) / Math.log10(Math.E));
     const TDD_sigmoid_exponent = tdd_dev * TDD_sigmoid_adjustment_factor + TDD_sigmoid_fix_offset;
-      var log_TDD_sigmoid_interval = "Log: TDD_sigmoid_interval: " + TDD_sigmoid_interval;
-      var log_TDD_sigmoid_max_minus_one = "Log: TDD_sigmoid_max_minus_one: " + TDD_sigmoid_max_minus_one;
-      var log_TDD_sigmoid_fix_offset = "Log: TDD_sigmoid_fix_offset: " + TDD_sigmoid_fix_offset;
-      var log_TDD_sigmoid_exponent = "Log: TDD_sigmoid_exponent: " + TDD_sigmoid_exponent;
+      log_TDD_sigmoid_interval = "Log: TDD_sigmoid_interval: " + TDD_sigmoid_interval;
+      log_TDD_sigmoid_max_minus_one = "Log: TDD_sigmoid_max_minus_one: " + TDD_sigmoid_max_minus_one;
+      log_TDD_sigmoid_fix_offset = "Log: TDD_sigmoid_fix_offset: " + TDD_sigmoid_fix_offset;
+      log_TDD_sigmoid_exponent = "Log: TDD_sigmoid_exponent: " + TDD_sigmoid_exponent;
        
     // The TDD Factor sigmoid function
     const tdd_factor = TDD_sigmoid_interval / (1 + Math.exp(-TDD_sigmoid_exponent)) + TDD_sigmoid_min;
-       var log_tdd_factor = "Log: tdd_factor: " + tdd_factor;
+       log_tdd_factor = "Log: tdd_factor: " + tdd_factor;
 
     // Adjust the stregnth of the TDD Factor; 100% = FULL TDD delta effect similar to Chris Wilson (Logarithmic) DynISF, 50% = half the effect, etc.
     const tdd_factor_strength_slider = 1;
-       var log_tdd_factor_strength_slider = "Log: tdd_factor: " + tdd_factor;
+       log_tdd_factor_strength_slider = "Log: tdd_factor: " + tdd_factor;
 
 
     // The user adjusted TDD factor based on above % slider
     const modified_tdd_factor = ((tdd_factor - 1) * tdd_factor_strength_slider) + 1;
-        var log_modified_tdd_factor = "Log: modified_tdd_factor: " + modified_tdd_factor;
+        log_modified_tdd_factor = "Log: modified_tdd_factor: " + modified_tdd_factor;
 
 
 // The Dynamic ISF Sigmoid Code 
@@ -123,10 +123,10 @@ function middleware(iob, temp_basal, glucose, profile, autosens, meal, reservoir
       const ratioInterval = maximumRatio - minimumRatio;
        var max_minus_one = maximumRatio - 1;
 
-      var log_minimumRatio = "Log: minimumRatio: " + minimumRatio;
-      var log_maximumRatio  = "Log: maximumRatio: " + maximumRatio;
-      var log_ratioInterval  = "Log: ratioInterval: " + ratioInterval;
-      var log_max_minus_one  = "Log: max_minus_one: " + max_minus_one;
+      log_minimumRatio = "Log: minimumRatio: " + minimumRatio;
+      log_maximumRatio  = "Log: maximumRatio: " + maximumRatio;
+      log_ratioInterval  = "Log: ratioInterval: " + ratioInterval;
+      log_max_minus_one  = "Log: max_minus_one: " + max_minus_one;
 
 
       // DYNISF SIGMOID MODIFICATION #2
@@ -137,30 +137,30 @@ function middleware(iob, temp_basal, glucose, profile, autosens, meal, reservoir
     // ORIGINAL SIGMOID APPROACH: const bg_dev = (current_bg - profile.min_bg) * 0.0555;
 
     const deviation = (myGlucose - (target / modified_tdd_factor)) * 0.0555; 
-       var log_deviation  = "Log: deviation: " + deviation;
+       log_deviation  = "Log: deviation: " + deviation;
        
      //Makes sigmoid factor(y) = 1 when BG deviation(x) = 0.
      const fix_offset = (Math.log10(1/max_minus_one-minimumRatio/max_minus_one) / Math.log10(Math.E));
-       var log_fix_offset  = "Log: fix_offset: " + fix_offset;
+       log_fix_offset  = "Log: fix_offset: " + fix_offset;
        
      //Exponent used in sigmoid formula
      const exponent = deviation * adjustmentFactor * modified_tdd_factor + fix_offset;
-       var log_exponent  = "Log: exponent: " + exponent;
+       log_exponent  = "Log: exponent: " + exponent;
        
      // The sigmoid function
      var sigmoidFactor = ratioInterval / (1 + Math.exp(-exponent)) + minimumRatio;
-       var log_sigmoidFactor  = "Log: sigmoidFactor: " + sigmoidFactor;
+       log_sigmoidFactor  = "Log: sigmoidFactor: " + sigmoidFactor;
        
      //Respect min/max ratios
      sigmoidFactor = Math.max(Math.min(maximumRatio, sigmoidFactor), sigmoidFactor, minimumRatio);
-       var logminmax_sigmoidFactor  = "Log: sigmoidFactor post min/max: " + sigmoidFactor;
+       logminmax_sigmoidFactor  = "Log: sigmoidFactor post min/max: " + sigmoidFactor;
 
       // Sets the new ratio
      autosens.ratio = sigmoidFactor;
-       var log_autosens.ratio  = "Log: New autosens.ratio: " + autosens.ratio;
+       log_autosens.ratio  = "Log: New autosens.ratio: " + autosens.ratio;
        
     const normal_cr = profile.carb_ratio;
-      var log_normal_cr = "Log: normal_cr: " + normal_cr;
+      log_normal_cr = "Log: normal_cr: " + normal_cr;
 
 
         // Dynamic CR. Use only when the setting 'Enable Dyanmic CR' is on in FAX Dynamic Settings
@@ -169,7 +169,7 @@ function middleware(iob, temp_basal, glucose, profile, autosens, meal, reservoir
         } else if (enableDynCR) { profile.carb_ratio /= autosens.ratio; }
 
         const new_isf = profile.sens/autosens.ratio;
-          var log_new_isf = "Log: new_isf: " + new_isf;
+          log_new_isf = "Log: new_isf: " + new_isf;
         
         return "Using Middleware function, the autosens ratio has been adjusted with sigmoid factor to: " + round(autosens.ratio, 2) + ". New ISF = " + round(new_isf, 2) + " mg/dl (" + round(0.0555 * new_isf, 2) + " (mmol/l)" + ". CR adjusted from " + round(normal_cr,2) + " to " + round(profile.carb_ratio,2) + " (" + round(0.0555 * profile.carb_ratio, 2) + " mmol/l).";
     } else { return "Nothing changed"; }
